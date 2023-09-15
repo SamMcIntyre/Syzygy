19c06e72232091fd43acd8b6bad1761a0696ab20
Samuel McIntyre <sammac1229@live.com>
2023-09-15 13:59:40 -0500

    last update

--- Assets/Scripts/Mechanics/SlingerContoller.cs
+++ Assets/Scripts/Mechanics/SlingerContoller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlingerContoller : MonoBehaviour
+public class SlingerContoller : MonoBehaviour, IPointerClickHandler
 {
 	//declare public vars
 	public Transform pTrans;
@@ -23,5 +23,11 @@ public class SlingerContoller : MonoBehaviour
 
     }
 
+	//Detect if a click occurs
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+	//Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
+		Debug.Log(name + " Game Object Clicked!");
+    }
 
 }
