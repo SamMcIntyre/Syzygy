3e76f47683c608e3252ceed69bbdc684586f69db
Samuel McIntyre <sammac1229@live.com>
2023-09-15 14:23:50 -0500

    inti
    start to look into using input actions

--- Assets/Scripts/Mechanics/SlingerContoller.cs
+++ Assets/Scripts/Mechanics/SlingerContoller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlingerContoller : MonoBehaviour, IPointerClickHandler
+public class SlingerContoller : MonoBehaviour
 {
 	//declare public vars
 	public Transform pTrans;
@@ -21,13 +21,6 @@ public class SlingerContoller : MonoBehaviour, IPointerClickHandler
     void Update()
     {
 
-    }
-
-	//Detect if a click occurs
-    public void OnPointerClick(PointerEventData pointerEventData)
-    {
-	//Output to console the clicked GameObject's name and the following message. You can replace this with your own actions for when clicking the GameObject.
-		Debug.Log(name + " Game Object Clicked!");
     }
 
 }
