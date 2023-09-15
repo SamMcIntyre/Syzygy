17a880b8b4f226234454a0c196640141bbb00b93
Samuel McIntyre <sammac1229@live.com>
2023-09-15 16:10:14 -0500

    add_line_render
    adding the line renderer so that the drag can be visualized, doesn't work yet

--- Assets/Scripts/Mechanics/SlingerContoller.cs
+++ Assets/Scripts/Mechanics/SlingerContoller.cs
@@ -1,26 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SlingerContoller : MonoBehaviour
 {
 	//declare public vars
 	public Transform pTrans;
 	public GameObject player;
+	public LineRenderer chargeLine;
 
 	//declare private vars
 	PlayerController pc;
+	Vector2 dragStartPos, dragEndPos;
+	bool mouseDown = false;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+	// Start is called before the first frame update
+	void Start()
+	{
 		pc = player.GetComponent<PlayerController>();
-    }
+		chargeLine.enabled = false;
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
+		//get mouse position (for potential later use)
+		Vector2 mousePos = Mouse.current.position.ReadValue();
 
-    }
+		//update line
+		if(mouseDown == true){
+			chargeLine.SetPosition(1, mousePos);
+		}
+
+
+		if (Mouse.current.leftButton.wasPressedThisFrame)
+		{
+			Debug.Log(mousePos);
+			mouseDown = true;
+			DragStart(mousePos);
+		}
+
+		if (Mouse.current.leftButton.wasReleasedThisFrame)
+		{
+			Debug.Log(mousePos);
+			mouseDown = false;
+			dragEndPos = mousePos;
+		}
+	}
+
+	void DragStart(Vector2 mousePos){
+		dragStartPos = mousePos;
+		chargeLine.SetPosition(0, dragStartPos);
+		chargeLine.SetPosition(1, new Vector2(3,3));
+		chargeLine.enabled = true;
+	}
 
 }
